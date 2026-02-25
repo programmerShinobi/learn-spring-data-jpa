fd71154895464c6a47029694cab470c776b38a31
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-25 13:16:46 +0700

    [add] setup project

---
+++ src/main/java/programmershinobi/belajar/springdata/jpa/BelajarSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package programmershinobi.belajar.springdata.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmershinobi/belajar/springdata/jpa/BelajarSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package programmershinobi.belajar.springdata.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
